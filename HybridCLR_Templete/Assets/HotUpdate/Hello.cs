b3fb1175dc942d69bd26f8f4aa9aa5ced5f8b734
GTuanTuan <1747715394@qq.com>
2024-01-04 10:29:49 +0800

    测试热更场景，脚本，配置

--- HybridCLR_Templete/Assets/HotUpdate/Hello.cs
+++ HybridCLR_Templete/Assets/HotUpdate/Hello.cs
@@ -10,6 +10,7 @@ public class Hello
 
         GameObject go = new GameObject("Test1");
         go.AddComponent<Print>();
+        FGUIManager.Instance().Init();
         AssetLoader.Instance().LoadSceneAsync("Assets/Res/Entity/Scene/Test/Test.unity", (scene) =>
         {
              Debug.Log($"load {scene.Result.Scene.name}");
